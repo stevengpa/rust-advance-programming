0c2050c96fa1276f2422943ec3507c6c4468e789
stevegpa <steven.gpa.git@gmail.com>
2025-11-23 21:44:01 -0600

    Practicing vectors

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod vectors_practice;
+
+use crate::vectors_practice::{vectors_practice};
+
+fn main() {
+    vectors_practice()
+}
---
+++ src/vectors_practice.rs
@@ -0,0 +1,48 @@
+
+pub fn vectors_practice() {
+    let mut vec1: Vec<i32> = Vec::new();
+    vec1.push(1);
+    vec1.push(3);
+    vec1.push(2);
+    vec1.push(5);
+    vec1.push(6);
+    vec1.push(7);
+    vec1.insert(0, -1);
+
+    println!("Vec1 {:?}", vec1);
+
+    // Sort
+    vec1.sort();
+    println!("Sorted Vec1 {:?}", vec1);
+    vec1.sort_by(|a, b|b.cmp(a));
+    println!("Sorted Vec1 {:?}", vec1);
+
+    // Remove
+    vec1.remove(4);
+    println!("Remove Vec1 4 index {:?}", vec1);
+    vec1.pop();
+    println!("Pop Vec1 {:?}", vec1);
+
+    // Slice
+    let top_3: &[i32] = &vec1[0..3];
+    println!("Top 3 {:?}", top_3);
+    println!("Vec1 {:?}", vec1);
+
+    // SubSetting (split, split_at)
+
+    // Split
+    let vec2 = vec![1, 2, 3, 4, 5];
+    let vec_split: Vec<&[i32]> = vec2.split(|&x| x == 3).collect();
+    println!("Vec2 {:?}", vec2);
+    println!("Vec Split {:?}", vec_split);
+
+    // Split At
+    let (vec_split_left, vec_split_right): (&[i32], &[i32]) = vec2.split_at(2);
+    println!("Vec2 {:?}", vec2);
+    println!("Vec Left {:?} - Vec Right {:?}", vec_split_left, vec_split_right);
+
+    // Iterate
+    for num in vec_split_left.iter() {
+        println!("Vec Left Num: {:?}", num);
+    }
+}
